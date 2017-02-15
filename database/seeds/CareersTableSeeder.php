5a8621fd948351d36ae410e6e945d76dd65f3f58
Unknown <brandonreyes230495@gmail.com>
2017-02-15 02:57:16 -0800

    A few more careers

--- database/seeds/CareersTableSeeder.php
+++ database/seeds/CareersTableSeeder.php
@@ -13,6 +13,26 @@ class CareersTableSeeder extends Seeder
     {
         $career = new \App\Career();
         $career->name = 'Tecnologías de la Información y Comunicación';
-        //$career->save();
+        $career->save();
+
+        $career = new \App\Career();
+        $career->name = 'Mecatrónica';
+        $career->save();
+
+        $career = new \App\Career();
+        $career->name = 'Desarrollo e Innovación Empresarial';
+        $career->save();
+
+        $career = new \App\Career();
+        $career->name = 'Energías Renovables';
+        $career->save();
+
+        $career = new \App\Career();
+        $career->name = 'Logística Comercial Global';
+        $career->save();
+
+        $career = new \App\Career();
+        $career->name = 'Electromecánica Industrial';
+        $career->save();
     }
 }
