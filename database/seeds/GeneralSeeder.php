effdf65cf982c8b8cc4ef84839f2b525477d1d13
Unknown <brandonreyes230495@gmail.com>
2017-02-15 18:34:53 -0800

    Refactor of GeneralSeeder

--- database/seeds/GeneralSeeder.php
+++ database/seeds/GeneralSeeder.php
@@ -14,27 +14,16 @@ class GeneralSeeder extends Seeder
         $faker = new \Faker\Generator();
         $faker->addProvider(new Faker\Provider\Base($faker));
 
-        \App\Career::all()->each(function ($c) use ($faker) {
+        \App\Career::all()->each(function ($career) use ($faker) {
 
             //two groups per career
 
             for($x = 0; $x <= 1; $x++) {
 
-                $shiftAbb = $faker->randomElement(['V', 'M']);
-                $grade = $x + 1;
-                $groupLetter = $faker->randomElement(['A', 'B', 'C']);
-
-                $shift = $shiftAbb == 'V' ? 'Vespertino' : 'Matutino';
-
-                //todo: el campo 'group' tendra que guardar solo la letra, ya no todo el texto, una vez hecho eso, se creara el model factory para refactorizar esta parte
-
-                $group = new \App\Group();
-                $group->shift = $shiftAbb;
-                $group->grade = $grade;
-                $group->group = $grade.'°'.$groupLetter.' - '.$shift;
-                $group->period_id = 2;
-                $group->career_id = $c->id;
-                $group->save();
+                $group = factory(\App\Group::class)->create([
+                    'grade' => $x + 1,
+                    'career_id' => $career->id
+                ]);
 
                 //two students per group
 
