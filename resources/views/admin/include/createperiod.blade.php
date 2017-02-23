0786b40d45764b3770e128acf9fc8656dcdb3c92
DaliaPinto <dalia.pinto2@gmail.com>
2017-02-22 17:26:39 -0800

    Working with php objects, learning vue

--- resources/views/admin/include/createperiod.blade.php
+++ resources/views/admin/include/createperiod.blade.php
@@ -1,50 +1,34 @@
-@section('css')
-    <link href="/css/jquery/jquery-ui.css" rel="stylesheet">
-@endsection
-    @if($period !=null)
-        <div class="row txt-align-center schedule-head">Horarios del período {{$period->description}}</div>
-    @else
-        <div class="col-xs-4">
-            Seleccione la fecha inicial del Periodo
-        </div>
-        <div class="col-xs-2">
-             <i class="fa fa-calendar" aria="true"></i>
-            <input type="text" class="" id="datepicker-start" placeholder="Selecciona la fecha de Inicio">
-        </div>
-        <div class="col-xs-4">
-            Seleccione la fecha final del primer mes del periodo
-        </div>
-        <div class="col-xs-2">
-             <i class="fa fa-calendar" aria="true"></i>
-            <input type="text" class="" id="datepicker-start" placeholder="Selecciona la fecha de Inicio">
-        </div>
-        <div class="col-xs-4">
-            Seleccione la inicial del último mes del Periodo
-        </div>
-        <div class="col-xs-2">
-            <i class="fa fa-calendar" aria="true"></i>
-            <input type="text" class="" id="datepicker-end" placeholder="Selecciona la fecha Fin">
-        </div>
-        <div class="col-xs-4">
-            Seleccione la fecha final del Periodo
-        </div>
-        <div class="col-xs-2">
-             <i class="fa fa-calendar" aria="true"></i>
-            <input type="text" class="" id="datepicker-end" placeholder="Selecciona la fecha Fin">
-        </div>
-    @endif
-@section('javascript')
-    <script src="{{URL::to('/js/jquery/jquery-1.10.2.js')}}" type="text/javascript"></script>
-    <script src="{{URL::to('/js/jquery-min/jquery.min.js')}}" type="text/javascript"></script>
-    <script src="{{URL::to('/js/jquery/jquery-ui.js')}}" type="text/javascript"></script>
+@if($period !=null)
+    <div class="row txt-align-center schedule-head">Horarios del período {{$period->description}}</div>
+@else
+    <div class="col-xs-4">
+        Seleccione la fecha inicial del Periodo
+    </div>
+    <div class="col-xs-2">
+         <i class="fa fa-calendar" aria="true"></i>
+        <input type="text" class="" id="datepicker-start" placeholder="Selecciona la fecha de Inicio">
+    </div>
+    <div class="col-xs-4">
+        Seleccione la fecha final del primer mes del periodo
+    </div>
+    <div class="col-xs-2">
+         <i class="fa fa-calendar" aria="true"></i>
+        <input type="text" class="" id="datepicker-start" placeholder="Selecciona la fecha de Inicio">
+    </div>
+    <div class="col-xs-4">
+        Seleccione la inicial del último mes del Periodo
+    </div>
+    <div class="col-xs-2">
+        <i class="fa fa-calendar" aria="true"></i>
+        <input type="text" class="" id="datepicker-end" placeholder="Selecciona la fecha Fin">
+    </div>
+    <div class="col-xs-4">
+        Seleccione la fecha final del Periodo
+    </div>
+    <div class="col-xs-2">
+         <i class="fa fa-calendar" aria="true"></i>
+        <input type="text" class="" id="datepicker-end" placeholder="Selecciona la fecha Fin">
+    </div>
+@endif
 
-    <script>
-        //datepicker
-        $(function() {
-            $("#datepicker-start, #datepicker-end").datepicker({
-                dateFormat: 'yy-mm-dd'
-            });
-        });
-    </script>
-@endsection
 
